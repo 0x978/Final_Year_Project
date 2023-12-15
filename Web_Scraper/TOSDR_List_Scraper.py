c197743f66287456f1c81fa4784249e11a5fd01b
Logan <cb58logan@gmail.com>
2023-12-15 06:45:11 +0000

    Rewrote TOSDR_Summary_Scraper to now scrape Terms And Conditions and Privacy Policy information and URLs.

--- Web_Scraper/TOSDR_List_Scraper.py
+++ Web_Scraper/TOSDR_List_Scraper.py
@@ -3,8 +3,11 @@ import requests
 from bs4 import BeautifulSoup
 import time
 
+
 # Scrapes a list of services for which TOSDR has summary points for.
 # The resulting list is 1695 services long.
+# Timeouts are used to not flood any website with requests
+# As a result this takes about 15 minutes to execute
 
 class TOSDRListScraper:
     page_number = 289  # Start page number at 289 - The first page with any useful summaries.
@@ -30,16 +33,17 @@ class TOSDRListScraper:
             table_items = soup.find_all(['tr'])  # create a list of all table rows on webpage
 
             for item in table_items:
+                # Searching for the element containing the URL to the service.
                 for child_item in item:
                     formatted_child = re.sub(r'\s+', ' ', str(child_item))  # Removing unnecessary spacing from child
 
                     # Ignore any services which do not have a rating, and thus, no summaries.
-                    if formatted_child == "<td> N/A </td>":
+                    if formatted_child == "<td> N/A </td>":  # Uniquely identifies unrated services
                         break
 
-                    if "href=\"/services" in formatted_child:
-                        service_number = re.search(service_number_regex, formatted_child)
-                        if service_number:
+                    if "href=\"/services" in formatted_child:  # If child element has a URL to a service
+                        service_number = re.search(service_number_regex, formatted_child)  # extract it,
+                        if service_number:  # Then write it to file.
                             self.write_to_file(service_number.group(0))
                             print(f'found service URL: {service_number.group(0)}')
                             count += 1
@@ -47,7 +51,7 @@ class TOSDRListScraper:
 
             print(f'Fetched {count} pages from {self.page_number} out of 357, '
                   f'waiting 7 seconds and moving to next page.')
-            time.sleep(7)
+            time.sleep(7)  # 7-Second delay, as to not flood any website with requests.
             self.page_number += 1
 
     def write_to_file(self, href):
