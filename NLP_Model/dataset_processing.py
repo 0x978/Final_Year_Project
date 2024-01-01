490fa80d7864479b158d773c1da3d07443dbf8a2
Logan <cb58logan@gmail.com>
2024-01-01 21:52:30 +0000

    Dataset processing for terms and conditions

--- NLP_Model/dataset_processing.py
+++ NLP_Model/dataset_processing.py
@@ -4,43 +4,43 @@ import json
 
 # Given the path to a web scraped dataset, turns the dataset into a JSON file, where each row in the JSON file contains
 # A privacy policy and its associated summary.
-def process_json(folder_path):
+def process_json(folder_path,document):
     results_list = []
 
     for folder in os.listdir(folder_path):
         nested_folder_path = os.path.join(folder_path, folder)
 
-        processed_document = process_document(nested_folder_path)
+        processed_document = process_document(nested_folder_path,document)
 
-        if "privacy_policy" in processed_document and "summary" in processed_document:
+        if "document" in processed_document and "summary" in processed_document:
             # append the returned privacy policy and summary to the list of results.
             results_list.append(
-                {"summary": processed_document["summary"], "document": processed_document["privacy_policy"]})
+                {"summary": processed_document["summary"], "document": processed_document["document"]})
 
     # Write the results list to JSON file.
-    with open("dataset.json", 'w') as json_file:
+    with open(f"{document}_dataset.json", 'w') as json_file:
         json.dump(results_list, json_file, indent=2)  # writes to json file "dataset.json"
 
 
-# Returns an object with the privacy policy and privacy policy summary in the given path.
-def process_document(path):
-    privacy_policy_data = {}
+# Returns an object with the document, and it's associated summary in an object
+def process_document(path,document):
+    data = {}
 
     # Create paths to privacy policy and privacy policy summary in current directory.
-    privacy_policy_path = os.path.join(path, "Privacy_Policy.txt")
-    summary_privacy_policy_path = os.path.join(path, "Privacy_Policy_Summary.txt")
+    document_path = os.path.join(path, f'{document}.txt')
+    summary_document_path = os.path.join(path, f'{document}_Summary.txt')
 
     # If paths to the privacy policy and its summary exists.
-    if os.path.exists(privacy_policy_path) and os.path.exists(summary_privacy_policy_path):
-        with open(privacy_policy_path, 'r', encoding="utf-8") as policy:
-            with open(summary_privacy_policy_path, 'r', encoding="utf-8") as summary:
+    if os.path.exists(document_path) and os.path.exists(summary_document_path):
+        with open(document_path, 'r', encoding="utf-8") as curr_doc:
+            with open(summary_document_path, 'r', encoding="utf-8") as summary:
                 # write the content of the privacy policy and summary to object
-                privacy_policy_data = {
-                    "privacy_policy": policy.read().strip(),
+                data = {
+                    "document": curr_doc.read().strip(),
                     "summary": summary.read().strip(),
                 }
 
-    return privacy_policy_data
+    return data
 
 
 # Returns number of Terms & Conditions and number of privacy policies in dataset
@@ -58,4 +58,4 @@ def count(folder_path):
 
 
 SCRAPED_DATA_PATH = "../Web_Scraper/scraped_data"
-process_json(SCRAPED_DATA_PATH)
+process_json(SCRAPED_DATA_PATH,"Terms")
