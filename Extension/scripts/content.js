98f6daedaeaa8d9a46b62622c23bd3a0937575ee
Logan <cb58logan@gmail.com>
2024-01-05 23:01:17 +0000

    Added interaction between extension and server

--- Extension/scripts/content.js
+++ Extension/scripts/content.js
@@ -2,7 +2,16 @@ chrome.runtime.onMessage.addListener(
     function(request) {
         if( request.message === "summarize_terms" ) {
             let pageContent = document.body.innerText;
-            console.log(pageContent);
+            receiveSummary(pageContent)
         }
     }
 );
+
+async function receiveSummary(document){
+    const res = await fetch("http://127.0.0.1:5000/summarise",{
+        method:"POST",
+        body: document
+    })
+    const summary = await res.json()
+    console.log(summary)
+}
