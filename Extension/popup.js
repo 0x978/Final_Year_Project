ac7937242ebe044a55034a277aba609766c1782d
Logan <cb58logan@gmail.com>
2024-01-06 00:11:38 +0000

    init button state

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let summarizeButton = document.getElementById('summarizeButton');
+    let privacyPolicyButton = document.getElementById('privacyPolicyButton');
 
     // Add listener to button
-    summarizeButton.addEventListener('click', async function () {
+    privacyPolicyButton.addEventListener('click', async function () {
         let tab = await getCurrentUserTab()
+        setButtonState(privacyPolicyButton,"loading...")
         chrome.tabs.sendMessage(tab.id, {"message": "summarize_terms"});
     });
 });
@@ -12,4 +13,11 @@ document.addEventListener('DOMContentLoaded', function() {
 async function getCurrentUserTab(){
     let [tab] = await chrome.tabs.query({ active: true, lastFocusedWindow: true });
     return tab;
+}
+
+// Set the text of a given button element in the popup.html UI.
+// State is to be an object with keys "Text" and "Active".
+function setButtonState(button,state){
+    console.log(button.innerText)
+    button.innerText = state
 }
