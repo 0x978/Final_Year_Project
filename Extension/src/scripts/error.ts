404fd97191345715aaf04c562a89f524a73fc332
Logan <cb58logan@gmail.com>
2024-01-19 01:04:39 +0000

    Bugfix in error.ts

--- Extension/src/scripts/error.ts
+++ Extension/src/scripts/error.ts
@@ -1,9 +1,10 @@
 const buttonElement:HTMLElement|null = document.getElementById("errorButton")
 
-
+// When the error page loads, assign a function to return back to the normal popup page to the button.
 document.addEventListener('DOMContentLoaded', function () {
     if(buttonElement){
         buttonElement.onclick = () => {
+            void chrome.runtime.sendMessage({"message": "setDefault"}); // Received by background.ts
             location.href = "../HTML/popup.html"
         }
     }
