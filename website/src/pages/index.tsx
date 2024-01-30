3ec130f02b86a3d3a3c8bdc2cb58e0a6cea75a4e
Logan Card <97891715+0x978@users.noreply.github.com>
2024-01-30 08:30:25 +0000

    Moved website to next.js & tailwind

---
+++ website/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import {FC} from "react"
+
+interface IndexProps {
+
+}
+
+const Index: FC<IndexProps> = ({}) => {
+    return(
+        <main className="flex h-screen text-center ">
+            <div className="m-auto">
+
+            </div>
+        </main>
+    )
+}
+
+export default Index
