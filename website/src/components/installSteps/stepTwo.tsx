0637e49b036f1b31b36af6ed7edb1917cd477cdb
Logan <cb58logan@gmail.com>
2024-02-08 17:23:10 +0000

    Improvements to instructions

--- website/src/components/installSteps/stepTwo.tsx
+++ website/src/components/installSteps/stepTwo.tsx
@@ -5,20 +5,24 @@ import stepTwoImage from "../../images/stepTwoImg.png"
 const stepTwo = () => {
 
     return (
-            <div className="text-center">
-                <h1 className={"text-4xl font-bold mb-4"}>Step Two:</h1>
-                <h2>Navigate to where you installed the file and extract it by right-clicking it and clicking "extract-all"</h2>
-                <h2>If done correctly, you should end up with two files, "PolicyPal.zip" and "PolicyPal".</h2>
+        <div className="text-center text-xl">
+            <h1 className={"text-4xl font-bold mb-4"}>Step Two:</h1>
+            <h2 className={"mb-2"}>Navigate to where you installed the file and extract it by right-clicking it and
+                clicking "extract-all"</h2>
+            <h2 className={"mb-4"}>If done correctly, you should end up with two files, "PolicyPal.zip" and
+                "PolicyPal".</h2>
+            <h2 className={"mb-2"}>(If using <b>macOS</b>, simply double click the downloaded file to extract it)</h2>
 
-                <div className={"flex justify-center align-middle"}>
-                    <div className={"h-1/2 w-1/2"}>
-                        <Image
-                            src={stepTwoImage}
-                            alt="Instructional image for extracting the file."
-                        />
-                    </div>
+
+            <div className={"flex justify-center align-middle"}>
+                <div className={"h-1/2 w-1/2"}>
+                    <Image
+                        src={stepTwoImage}
+                        alt="Instructional image for extracting the file."
+                    />
                 </div>
             </div>
+        </div>
     )
 }
 
--- website/src/components/usageSteps/stepThree.tsx
+++ website/src/components/usageSteps/stepThree.tsx
@@ -10,11 +10,12 @@ const UsageStepTwo: FC = ({}) => {
     return (
         <div className="text-center">
             <h1 className={"text-4xl font-bold mb-4"}>Step Three:</h1>
+            <h2 className={"text-xl mb-2"}>Whilst on a terms and conditions or privacy policy page:</h2>
             <h2 className={"text-xl mb-2"}>Press the extension button, which looks like a puzzle piece in the top
                 right (1)</h2>
             <h2 className={"text-xl mb-2"}>Then press "policyPal" (2)</h2>
             <h2 className={"text-xl mb-8"}>Optionally, you may also pin the extension for easier access in the future
-                    (3)</h2>
+                (3)</h2>
 
             <div className={"flex justify-center align-middle"}>
                 <div className={"h-1/2 w-1/2"}>
